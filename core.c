fdf8f4aac973ae28383ef4be5f589bb79dc59f27
Oussama Kamili <oussama.kamili@protonmail.com>
2023-05-13 02:56:18 +0100

    counting arguments, work in progress

--- core.c
+++ core.c
@@ -1,5 +1,40 @@
 #include "core.h"
 
+
+int	args_count(char *cmd)
+{
+	int	count;
+	int	dquotes;
+	int	squotes;
+
+	count = 0;
+	dquotes = 0;
+	squotes = 0;
+	while (cmd[0])
+	{
+		if(ft_strchr("><", cmd[0]) && (!(dquotes & 1) && !(squotes & 1)))
+		{
+			while (ft_strchr("><", cmd[0]))
+				cmd++;
+			count--;
+		}
+		if (!ft_strchr("\t |;", cmd[0]) && (!(dquotes & 1) && !(squotes & 1)))
+		{
+			count++;
+			while(!ft_strchr("\t ><", cmd[0]) || ((dquotes & 1) || (squotes & 1)))
+			{
+				dquotes += (!(squotes & 1) && cmd[0] == '"');
+				squotes += (!(dquotes & 1) && cmd[0] == '\'');
+				cmd++;
+			}
+		}
+		else
+			cmd++;
+	}
+	return count;
+}
+
+
 int main() {
 	t_cmd *cmds;
     char* input;
@@ -10,7 +45,7 @@ int main() {
 		cmds = input_split(input);
 		while (cmds)
 		{
-			printf("%d= %s\n", ++i, cmds->cmd);
+			printf("%d= %s (length: %d)\n", ++i, cmds->cmd, args_count(cmds->cmd));
 			cmds = remove_cmd_from_tree(cmds, cmds);
 		}
 		i = 0;
