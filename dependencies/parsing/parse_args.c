e03ba7f0d99a6a397bc23b30dba2fdd26b12c820
Oussama Kamili <oussama.kamili@protonmail.com>
2023-05-13 04:50:29 +0100

    done working on the function to count the arguments

--- dependencies/parsing/parse_args.c
+++ dependencies/parsing/parse_args.c
@@ -1,74 +1,41 @@
-# include "parsing.h"
+#include "parsing.h"
 
-int next_arg(char *cmd)
+char	*next_arg(char *cmd)
 {
-	int index;
 	int	dquotes;
 	int	squotes;
 
-	index = 0;
 	dquotes = 0;
 	squotes = 0;
-	while(cmd[index] && (!ft_strchr("\t |;", cmd[index]) || (dquotes & 1) || (squotes & 1)))
+	while (!ft_strchr("\t ><", cmd[0]) || ((dquotes & 1) || (squotes & 1)))
 	{
-		dquotes += (!(squotes & 1) && cmd[index] == '"');
-		squotes += (!(dquotes & 1) && cmd[index] == '\'');
-		index++;
+		dquotes += (!(squotes & 1) && cmd[0] == '"');
+		squotes += (!(dquotes & 1) && cmd[0] == '\'');
+		cmd++;
 	}
-	while (cmd[index] && (ft_strchr("\t |;", cmd[index])))
-		index++;
-	return index;
+	return (cmd);
 }
 
-int args_count(char *cmd)
+int	args_count(char *cmd)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	while (cmd[0])
 	{
-		if (!ft_strchr("\t |;", cmd[0]))
+		if (ft_strchr("><", cmd[0]))
+		{
+			while (ft_strchr("><", cmd[0]))
+				cmd++;
+			count--;
+		}
+		else if (!ft_strchr("\t |;", cmd[0]))
 		{
 			count++;
-			cmd += next_arg(cmd);
+			cmd = next_arg(cmd);
 		}
+		else
+			cmd++;
 	}
-	return count;
+	return (count);
 }
-
-
-
-
-
-
-
-// int	args_count(char *cmd)
-// {
-// 	int	count;
-// 	int	dquotes;
-// 	int	squotes;
-
-// 	count = 0;
-// 	while (cmd[0])
-// 	{
-// 		if(ft_strchr("><", cmd[0]) && (!(dquotes & 1) && !(squotes & 1)))
-// 		{
-// 			while (ft_strchr("><", cmd[0]))
-// 				cmd++;
-// 			count--;
-// 		}
-// 		if (!ft_strchr("\t |;", cmd[0]) && (!(dquotes & 1) && !(squotes & 1)))
-// 		{
-// 			count++;
-// 			while(!ft_strchr("\t ><", cmd[0]) || ((dquotes & 1) || (squotes & 1)))
-// 			{
-// 				dquotes += (!(squotes & 1) && cmd[0] == '"');
-// 				squotes += (!(dquotes & 1) && cmd[0] == '\'');
-// 				cmd++;
-// 			}
-// 		}
-// 		else
-// 			cmd++;
-// 	}
-// 	return count;
-// }
