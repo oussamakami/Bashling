c1780c3ac4393a88f341dad4734c471a309c4b75
Oussama Kamili <oussama.kamili@protonmail.com>
2023-06-16 14:53:31 +0100

    writing definition to each function

--- dependencies/executions/executions.h
+++ dependencies/executions/executions.h
@@ -6,7 +6,7 @@
 /*   By: okamili <okamili@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:48:10 by okamili           #+#    #+#             */
-/*   Updated: 2023/06/09 17:33:14 by okamili          ###   ########.fr       */
+/*   Updated: 2023/06/16 14:52:59 by okamili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,93 @@
 # include "../modules/modules.h"
 # include "../parsing/parsing.h"
 
+/**
+ * @brief Print the current working directory.
+ */
 void	pwd(void);
+/**
+ * @brief Navigate to a different working directory.
+ * 
+ * @param cmd Reference to the node for the cd command.
+ */
 void	cd(t_cmd *cmd);
+/**
+ * @brief Output the text to the standard output (STDOUT).
+ * 
+ * @param cmd Reference to the node for the echo command.
+ */
 void	echo(t_cmd *cmd);
+/**
+ * @brief Free allocated memory followed by terminating the program.
+ * 
+ * @param cmd Reference to the node for the exit command.
+ */
 void	close_prgm(t_cmd *cmd);
+/**
+ * @brief Print the list of env variables to standard output (STDOUT).
+ */
 void	list_env(void);
+/**
+ * @brief Unset one or more env variable.
+ * 
+ * @param cmd Reference to the node for the unset command.
+ */
 void	unset_env(t_cmd *cmd);
+/**
+ * @brief Either list or define one or more env variable.
+ * 
+ * @param cmd Reference to the node for the export command.
+ */
 void	run_export(t_cmd *cmd);
+/**
+ * @brief Execute built-in commands.
+ * 
+ * @param cmd Reference to the command node.
+ */
 void	run_builtins(t_cmd *cmd);
+/**
+ * @brief Execute system commands.
+ * 
+ * @param cmd Reference to the command node.
+ */
 void	run_commands(t_cmd *cmd);
+/**
+ * @brief Execute commands that are piping to each others.
+ * 
+ * @param cmds Reference to the first command node.
+ * @param err Reference to a variable that will contain the error
+ * return code of this execution.
+ * @return t_cmd* Reference to the last command node that used pipe.
+ */
 t_cmd	*run_pipe_commands(t_cmd *cmds, int *err);
+/**
+ * @brief Redirect data file descriptors according to the specified redirections.
+ * 
+ * @param cmd Reference to the command node.
+ */
 void	handle_redir(t_cmd *cmd);
+/**
+ * @brief Redirect data file descriptors according to the pipe file descriptors.
+ * 
+ * @param pfd Pipe file descriptors.
+ * @param red[2]: A switch that determines the redirection of which data stream,
+ * Set each index to 1 for true or 0 for false.
+ * (index0) for input and (index1) for output.
+ * @param newpfd another pipe file descriptors
+ * if you need the first pipe to redirect to this pipe.
+ */
 void	handle_fds(int pfd[2], int red[2], int newpfd[2]);
+/**
+ * @brief A function at a low level that carries out command execution.
+ * 
+ * @param cmd Reference to the command node.
+ * @param pfd Pipe file descriptors.
+ * @param red[2]: A switch that determines the redirection of which data stream,
+ * Set each index to 1 for true or 0 for false.
+ * (index0) for input and (index1) for output.
+ * @param newpfd another pipe file descriptors
+ * if you need the first pipe to redirect to this pipe.
+ */
 void	execute_cmd(t_cmd *cmd, int pfd[2], int red[2], int newpfd[2]);
 
 #endif
--- dependencies/modules/modules.h
+++ dependencies/modules/modules.h
@@ -6,7 +6,7 @@
 /*   By: okamili <okamili@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:49:02 by okamili           #+#    #+#             */
-/*   Updated: 2023/06/09 12:35:22 by okamili          ###   ########.fr       */
+/*   Updated: 2023/06/16 15:19:40 by okamili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,39 @@ typedef struct env_vars
 	struct env_vars	*next;
 }	t_env;
 
+/**
+ * @brief A global variable that stores the reference to
+ * the linked list encompassing all environment variables.
+ */
 extern t_env	*g_env;
+/**
+ * @brief Modify the size of a previously allocated memory.
+ * 
+ * @param ptr Pointer to the memory block that you wish to resize.
+ * @param size The new memory size.
+ * @return void* Pointer to a newly allocated memory block with the updated size.
+ */
 void	*ft_realloc(void *ptr, size_t size);
+/**
+ * @brief Retrieve the value of an environment variable by its name.
+ * 
+ * @param var_name The identifier/name of the environment variable.
+ * @return char* The corresponding value of that environment variable.
+ */
+char	*fetch(char *var_name);
 char	*get_work_dir(void);
 char	*get_host_name(void);
-char	*prompt(void);
+char	*extract_branch(char *buff);
 char	*prompt_msg(void);
-void	free2d(void **ptr);
+char	*prompt(void);
 char	*replace_word(char *str, char *w0, char *w1, int usefree);
 char	*replace_all_words(char *str, char *w0, char *w1, int usefree);
+t_env	*add_env(t_env *head, char *name, char *value);
 t_env	*last_env(t_env *head);
 t_env	*find_env(t_env *head, char *name);
-t_env	*add_env(t_env *head, char *name, char *value);
 void	destory_all_env(t_env *head);
-char	*fetch(char *var_name);
+void	free2d(void **ptr);
 void	env_init(int argc, char **argv, char **env);
 char	**export_env(void);
-char	*extract_branch(char *buff);
 
 #endif
