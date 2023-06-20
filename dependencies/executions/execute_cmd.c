b7c402b3e5f79ec9b4555fe10dcc4dd3acd97142
Oussama Kamili <oussama.kamili@protonmail.com>
2023-06-20 18:58:30 +0100

    add the ability to execute bash files

--- dependencies/executions/execute_cmd.c
+++ dependencies/executions/execute_cmd.c
@@ -6,12 +6,41 @@
 /*   By: okamili <okamili@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:47:23 by okamili           #+#    #+#             */
-/*   Updated: 2023/06/16 15:59:57 by okamili          ###   ########.fr       */
+/*   Updated: 2023/06/20 18:55:14 by okamili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executions.h"
 #include "../../core.h"
+
+static void	execute_bash_executables(t_cmd	*cmd, char **env)
+{
+	int		fd;
+	int		size;
+	char	*buffer;
+	char	*args[3];
+
+	fd = open(cmd->exec, O_RDONLY);
+	if (fd == 1)
+		return ;
+	buffer = ft_calloc(250, 1);
+	size = read(fd, buffer, 249);
+	close(fd);
+	buffer[size - 1] = '\0';
+	fd = ft_strlen(buffer);
+	buffer = replace_word(buffer, "/bin/bash", NULL, 1);
+	if (fd > (int)ft_strlen(buffer))
+	{
+		free(buffer);
+		args[0] = "bash";
+		args[1] = cmd->exec;
+		args[2] = NULL;
+		execve("/bin/bash", args, env);
+		free2d((void **)env);
+	}
+	else
+		free(buffer);
+}
 
 static void	execute_process(t_cmd *cmd, int pfd[2], int red[2], int newpfd[2])
 {
@@ -26,6 +55,7 @@ static void	execute_process(t_cmd *cmd, int pfd[2], int red[2], int newpfd[2])
 	else if (cmd->exec)
 	{
 		env = export_env();
+		execute_bash_executables(cmd, env);
 		execve(cmd->exec, cmd->args, env);
 		free2d((void **)env);
 	}
